337e3e9da4bf517c22eaa656134e5e8f3ecfc093
Vladislav Sapegin <sapegin@rutoken.ru>
2024-03-26 19:52:08 +0300

    Add lab3

---
+++ lab3/ca/ca.py
@@ -0,0 +1,337 @@
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.x509.oid import NameOID
+from cryptography.exceptions import InvalidSignature
+import datetime
+
+
+class CertificationAuthority:
+    def __init__(self):
+        self.certificates = []
+        self.revoked_certificates = set()
+        self.private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+            backend=default_backend()
+        )
+        self.root_certificate = self._create_root_certificate()
+
+    def _create_root_certificate(self):
+        builder = (
+            x509.CertificateBuilder()
+            .subject_name(x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, "Certificate Authority")]))
+            .issuer_name(x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, "Certificate Authority")]))
+            .public_key(self.private_key.public_key())
+            .serial_number(x509.random_serial_number())
+            .not_valid_before(datetime.datetime.utcnow())
+            .not_valid_after(datetime.datetime.utcnow() + datetime.timedelta(days=365))
+            .add_extension(
+                x509.BasicConstraints(ca=True, path_length=None),
+                critical=True,
+            )
+        )
+        return builder.sign(
+            private_key=self.private_key, algorithm=hashes.SHA256(), backend=default_backend()
+        )
+
+    def sign_certificate(self, public_key, subject_name):
+        for certificate in self.certificates:
+            if certificate.subject.rfc4514_string() == subject_name:
+                return None
+
+        for _,  _, revoked_public_key in self.revoked_certificates:
+            if public_key.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo) == revoked_public_key:
+                return None
+
+        builder = (
+            x509.CertificateBuilder()
+            .subject_name(x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, subject_name)]))
+            .issuer_name(self.root_certificate.subject)
+            .public_key(public_key)
+            .serial_number(x509.random_serial_number())
+            .not_valid_before(datetime.datetime.utcnow())
+            .not_valid_after(datetime.datetime.utcnow() + datetime.timedelta(days=365))
+        )
+        certificate = builder.sign(
+            private_key=self.private_key, algorithm=hashes.SHA256(), backend=default_backend()
+        )
+
+        self.certificates.append(certificate)
+
+        return certificate, self.root_certificate
+
+    def revoke_certificate(self, certificate_serial):
+        for i, certificate in enumerate(self.certificates):
+            if certificate.serial_number == certificate_serial:
+                self.certificates.pop(i)
+                self.revoked_certificates.add((certificate_serial, datetime.datetime.utcnow(), certificate.public_key().public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo)))
+                break
+
+
+    def is_certificate_revoked(self, certificate_serial):
+        return certificate_serial in self.revoked_certificates
+
+    def get_revoked_certificates(self):
+        crl_builder = (
+            x509.CertificateRevocationListBuilder()
+            .issuer_name(self.root_certificate.issuer)
+            .last_update(datetime.datetime.utcnow())
+            .next_update(datetime.datetime.utcnow() + datetime.timedelta(days=1))
+        )
+        for serial_number, revoke_time, _ in self.revoked_certificates:
+            crl_builder = crl_builder.add_revoked_certificate(
+                x509.RevokedCertificateBuilder()
+                .serial_number(serial_number)
+                .revocation_date(revoke_time)
+                .build()
+            )
+        crl = crl_builder.sign(
+            private_key=self.private_key,
+            algorithm=hashes.SHA256(),
+            backend=default_backend()
+        )
+        return crl
+
+    def validate_certificate(self, certificate):
+        try:
+            self.root_certificate.public_key().verify(
+                certificate.signature,
+                certificate.tbs_certificate_bytes,
+                padding.PKCS1v15(),
+                certificate.signature_hash_algorithm,
+            )
+        except InvalidSignature:
+            return False
+
+        if self.is_certificate_revoked(certificate.serial_number):
+            return False
+
+        return True
+
+    def process_certificate_request(self, csr):
+        public_key = csr.public_key()
+
+        try:
+            public_key.verify(
+                csr.signature,
+                csr.tbs_certrequest_bytes,
+                padding.PKCS1v15(),
+                csr.signature_hash_algorithm,
+            )
+        except InvalidSignature:
+            return None, None
+
+        certificate = self.sign_certificate(public_key, csr.subject.rfc4514_string())
+        return certificate
+
+
+class User:
+    def __init__(self, name, ca):
+        self.name = name
+        self.private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+            backend=default_backend()
+        )
+        self.public_key = self.private_key.public_key()
+        self.certificate = None
+
+        self.ca_root_certificate = ca.root_certificate
+        self.ca = ca
+
+    def generate_certificate_signing_request(self):
+        builder = (
+            x509.CertificateSigningRequestBuilder()
+            .subject_name(x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, self.name)]))
+            .add_extension(
+                x509.SubjectAlternativeName([x509.DNSName(self.name)]), critical=False
+            )
+        )
+        csr = builder.sign(
+            private_key=self.private_key, algorithm=hashes.SHA256(), backend=default_backend()
+        )
+        return csr
+
+    def generate_certificate_signing(self):
+        csr = self.generate_certificate_signing_request()
+        result_generate = self.ca.process_certificate_request(csr)
+        if not result_generate:
+            return False
+
+        self.certificate, _ = result_generate
+        return True
+
+    def get_certificate(self):
+        if not self.certificate:
+            raise ValueError("The certificate has not been received yet")
+
+        return self.certificate
+
+    def verify_certificate(self, certificate):
+        try:
+            crl = self.ca.get_revoked_certificates()
+            
+            # Проверяем подпись CRL с использованием открытого ключа корневого сертификата УЦ
+            self.ca_root_certificate.public_key().verify(
+                crl.signature,
+                crl.tbs_certlist_bytes,
+                padding.PKCS1v15(),
+                crl.signature_hash_algorithm,
+            )
+
+            if crl.get_revoked_certificate_by_serial_number(certificate.serial_number) is not None:
+                return False
+
+            self.ca_root_certificate.public_key().verify(
+                certificate.signature,
+                certificate.tbs_certificate_bytes,
+                padding.PKCS1v15(),
+                certificate.signature_hash_algorithm,
+            )
+            return True
+        except InvalidSignature:
+            return False
+
+    def revoke_certificate(self):
+        if self.certificate:
+            self.ca.revoke_certificate(self.certificate.serial_number)
+
+    def sign_data(self, data):
+        signature = self.private_key.sign(
+            data,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        return signature
+
+    def verify_data(self, data, signature, issuer_certificate):
+        if not self.verify_certificate(issuer_certificate):
+            print("Failed verify issuer certificate")
+            return False
+
+        try:
+            issuer_certificate.public_key().verify(
+                signature,
+                data,
+                padding.PSS(
+                    mgf=padding.MGF1(hashes.SHA256()),
+                    salt_length=padding.PSS.MAX_LENGTH
+                ),
+                hashes.SHA256()
+            )
+        except InvalidSignature:
+            print("Failed verify signature")
+            return False
+
+        return True
+
+
+def generate_fake_certificate():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+
+    subject = x509.Name([
+           x509.NameAttribute(NameOID.COMMON_NAME, "Fake"),
+    ])
+    builder = (
+        x509.CertificateBuilder()
+        .subject_name(subject)
+        .issuer_name(subject)
+        .public_key(private_key.public_key())
+        .serial_number(x509.random_serial_number())
+        .not_valid_before(datetime.datetime.utcnow())
+        .not_valid_after(datetime.datetime.utcnow() + datetime.timedelta(days=365))
+        .add_extension(
+            x509.BasicConstraints(ca=False, path_length=None),
+            critical=True,
+        )
+    )
+
+    certificate = builder.sign(
+        private_key=private_key,
+        algorithm=hashes.SHA256(),
+        backend=default_backend()
+    )
+
+    return certificate
+
+def main_ca():
+    print("Init CA, Bob and Alice...")
+    ca = CertificationAuthority()
+    alice = User("Alice", ca)
+    bob = User("Bob", ca)
+    print("Success init CA, Bob and Alice")
+
+    print("\nGenerate Alice certificate...")
+    if alice.generate_certificate_signing():
+        print("Success Alice generate_certificate_signing")
+    else:
+        print("Failed Bob generate_certificate_signing")
+        return
+
+    print("\nGenerate Bob certificate...")
+    if bob.generate_certificate_signing():
+        print("Success Bob generate_certificate_signing")
+    else:
+        print("Failed Bob generate_certificate_signing")
+        return
+
+    byte_array = b'sometextwrittingsometextwritting'
+
+    signature = bob.sign_data(byte_array)
+    
+    if alice.verify_data(byte_array, signature, bob.get_certificate()):
+        print("\nAlice verify byte array Bob")
+    else:
+        print("\nAlice failed verify byte array Bob")
+
+    print("\nVerify Alice and Bob certificate...")
+    if bob.verify_certificate(alice.certificate) and alice.verify_certificate(bob.certificate):
+        print("Certificates are valid.")
+
+        print("\nRevoke Alice certificate...")
+        alice.revoke_certificate()
+        if not bob.verify_certificate(alice.certificate):
+            print("Alice Certificate has been revoked")
+        else:
+            print("Alice Certificate has not been revoked")
+
+        if not alice.generate_certificate_signing():
+            print("Succes: fail to regenerate certificate")
+        else:
+            print("Failed: Alice can regenerate revoked certificate")
+    else:
+        print("Certificates are not valid.")
+
+    print("\nVerify fake certificate...")
+    if not bob.verify_certificate(generate_fake_certificate()):
+        print("Found fake certificate") 
+
+    # Получение объекта CRL
+    crl = ca.get_revoked_certificates()
+
+    # Распарсивание содержимого CRL
+    parsed_crl = x509.load_pem_x509_crl(crl.public_bytes(serialization.Encoding.PEM), default_backend())
+
+    # Извлечение серийных номеров отозванных сертификатов
+    serial_numbers = [entry.serial_number for entry in parsed_crl]
+
+    print("\nCRL")
+    # Вывод серийных номеров отозванных сертификатов
+    for serial_number in serial_numbers:
+        print("Serial Number:", serial_number)
+
+    print(crl.public_bytes(serialization.Encoding.PEM).decode())
+
+if __name__ == '__main__':
+    main_ca()
---
+++ lab3/lab3.py
@@ -0,0 +1,195 @@
+from ca import CertificationAuthority, User, generate_fake_certificate
+
+import random
+import numpy
+import string
+import hashlib
+from numpy.linalg import inv
+
+
+class DispersalAlgorithm:
+    @staticmethod
+    def generate_vandermonds_matrix(col : int, row : int, indx_list = None):
+        field = 2 ** 8
+
+        A = list()
+        for i in range(2, row + 2):
+            a = list()
+            for j in range(0, col):
+                a.append(pow(i, j, field))
+            A.append(a)
+
+        if indx_list == None:
+            return A
+
+    def __init__(self, m : int, n : int):
+        self.m, self.n = m, n
+        self.A = self.generate_vandermonds_matrix(m, n)
+
+    def dispersal(self, data : bytes):
+        M = list()
+
+        for i in range(0, len(data) //self.m):
+            S = list()
+            for j in range(0, self.m):
+                S.append(data[(i) * self.m + j])
+            M.append(S)
+        M = numpy.array(M).transpose()
+
+        F = numpy.matmul(self.A, M)
+
+        return F
+
+
+    def recovery(self, F, indx_list):
+        A_of_indx = list()
+        for i in indx_list:
+            A_of_indx.append(self.A[i])
+
+        A = inv(A_of_indx)
+        M = numpy.matmul(A, numpy.array(F))
+
+        message = ''
+        M = M.transpose()
+        for item_1 in M:
+            for item_2 in item_1:
+                message += chr(round(item_2))
+
+        return bytes(message, 'utf-8')
+
+
+class Gateway(User):
+    def __init__(self, storage_devices, dispersal_algorithm, name, ca):
+        User.__init__(self, name, ca)
+        self.dispersal_algorithm = dispersal_algorithm
+        self.storage_devices = storage_devices
+
+    def set_data(self, data, signature, issuer_certificate):
+        if not self.verify_data(data, signature, issuer_certificate):
+            return
+
+        for i in range(len(self.storage_devices)):
+            self.storage_devices[i].set_data(data, signature, issuer_certificate)
+    
+    def get_data_from_part(self):
+        hash_array = list()
+        hash_array_count = list()
+
+        for i in range(len(self.storage_devices)):
+            hash_array.append(0)
+            hash_array_count.append(0)
+
+        for device in self.storage_devices:
+            hash_item = device.get_part()[1]
+            for indx in range(len(self.storage_devices)):
+                if hash_array[indx] == 0:
+                    hash_array[indx] = hash_item[indx]
+                    hash_array_count[indx] = 1
+                    continue
+                hash_array_count[indx] += 1
+        return_part_array = list()
+        index_return_part_array = list()
+        for indx in range(len(self.storage_devices)):
+            if len(return_part_array) == self.dispersal_algorithm.m:
+                break
+            if hash_array_count[indx] == max(hash_array_count):
+                return_part_array.append(self.storage_devices[indx].get_part()[0])
+                index_return_part_array.append(indx)
+        
+        if len(return_part_array) < self.dispersal_algorithm.m:
+            raise ValueError(f'Recovery failed. Not much number {self.dispersal_algorithm.m} Storage device')
+
+        return self.dispersal_algorithm.recovery(return_part_array, index_return_part_array)
+
+
+class StorageDevice(User):
+    def __init__(self, identificator, dispersal_algorithm, name, ca):
+        User.__init__(self, name, ca)
+        self.identificator = identificator
+        self.dispersal_algorithm = dispersal_algorithm
+        self.part_list = None
+
+    def set_data(self, data, signature, issuer_certificate):
+        if not self.verify_data(data, signature, issuer_certificate):
+            return
+
+        parts = self.dispersal_algorithm.dispersal(data)
+        hash_parts = list()
+        for item in parts:
+            hash_object = hashlib.sha256()
+            for item_numpy in item:
+                hash_object.update(str(item_numpy).encode())
+            hash_parts.append(hash_object.digest())
+
+        self.part_list = (parts[self.identificator], hash_parts)
+
+    def get_part(self):
+        return self.part_list 
+
+
+def generate_random_ascii(length):
+    ascii_characters = string.ascii_letters + string.digits + string.punctuation + string.whitespace
+    random_ascii = ''.join(random.choice(ascii_characters[:127]) for _ in range(length))
+    return random_ascii.encode('ascii')
+
+
+def main():
+    n = 8
+    m = 4
+    dispersal_algorithm = DispersalAlgorithm(m, n)
+
+    byte_array = generate_random_ascii(n*m)
+
+    print(byte_array)
+
+    ca = CertificationAuthority()
+    alice = User("Alice", ca)
+
+    print("\nGenerate Alice certificate...")
+    if alice.generate_certificate_signing():
+        print("Success Alice generate_certificate_signing")
+    else:
+        print("Failed Alice generate_certificate_signing")
+        return
+
+    sd_list = list()
+    for i in range(0, n):
+        name = f"StorageData{i}"
+        sd = StorageDevice(i, dispersal_algorithm, name, ca)
+        
+        print(f"\nGenerate {name} certificate...")
+        if alice.generate_certificate_signing():
+            print(f"Success {name} generate_certificate_signing")
+        else:
+            print(f"Failed {name} generate_certificate_signing")
+            return
+
+        sd_list.append(sd)
+
+    gw = Gateway(sd_list, dispersal_algorithm, "Gw", ca)
+
+    print("\nGenerate Gateway certificate...")
+    if gw.generate_certificate_signing():
+        print("Success Gateway generate_certificate_signing")
+    else:
+        print("Failed Gateway generate_certificate_signing")
+        return
+
+    signature = alice.sign_data(byte_array)
+    print("\nSend data to Gateway")
+    gw.set_data(byte_array, signature, alice.get_certificate())
+    print("\nSuccess data stored on storage device")
+
+    print("\nGet data from Gateway")
+    recovery_data = gw.get_data_from_part()
+    print(byte_array, "==", recovery_data, recovery_data==byte_array)
+
+    print("Negative Test:")
+    # gw.storage_devices = gw.storage_devices[:3]
+    # recovery_data = gw.get_data_from_part()
+
+    # gw.set_data(byte_array, signature, gw.get_certificate())
+    gw.set_data(byte_array, signature, generate_fake_certificate())
+
+if __name__ == '__main__':
+    main()
